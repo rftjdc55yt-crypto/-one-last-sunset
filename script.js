2609ea16bbeb3170c9dea137ea3b879a1e8f341b
rftjdc55yt-crypto <rftjdc55yt@privaterelay.appleid.com>
2026-07-01 18:03:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const mulai = document.getElementById("mulai");
 const loading = document.getElementById("loading");
 const hero = document.getElementById("hero");
+const typing = document.getElementById("typing");
 
-mulai.addEventListener("click", () => {
+const text =
+"Hai Tria... Ada sesuatu yang ingin aku sampaikan. Tolong baca sampai selesai ya.";
+
+let i = 0;
+
+mulai.onclick = () => {
     loading.style.display = "none";
     hero.style.display = "flex";
-});
+
+    ketik();
+};
+
+function ketik(){
+
+    if(i < text.length){
+
+        typing.innerHTML += text.charAt(i);
+
+        i++;
+
+        setTimeout(ketik,40);
+
+    }
+
+}
