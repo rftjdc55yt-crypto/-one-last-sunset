3fc3a836cc9ce099cef7e13588f0c40b35eb473d
rftjdc55yt-crypto <rftjdc55yt@privaterelay.appleid.com>
2026-07-01 18:09:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,78 @@
-const mulai = document.getElementById("mulai");
 const loading = document.getElementById("loading");
 const hero = document.getElementById("hero");
+const surat = document.getElementById("surat");
+
+const mulai = document.getElementById("mulai");
+const lanjut = document.getElementById("lanjut");
+
+const judul = document.getElementById("judul");
 const typing = document.getElementById("typing");
+const isiSurat = document.getElementById("isiSurat");
+
+const title = "Hai, Tria.";
 
 const text =
-"Hai Tria... Ada sesuatu yang ingin aku sampaikan. Tolong baca sampai selesai ya.";
+"Aku tahu mungkin kamu tidak menyangka membuka link ini. Tapi izinkan aku menyampaikan beberapa hal yang selama ini tersimpan. Bacalah sampai selesai ya...";
 
-let i = 0;
+const suratText = `
+Tria,
+
+Terima kasih pernah menjadi bagian dari hidupku.
+
+Aku sadar dulu aku banyak melakukan kesalahan.
+Aku terlalu egois, terlalu kekanak-kanakan,
+dan sering tidak menghargai apa yang sudah aku miliki.
+
+Hari ini aku bukan datang untuk memaksamu kembali.
+
+Aku hanya ingin menunjukkan bahwa aku sudah belajar,
+bertumbuh, dan menjadi pribadi yang jauh lebih baik.
+
+Kalau suatu hari nanti takdir mempertemukan kita lagi,
+aku berharap saat itu aku sudah menjadi seseorang
+yang benar-benar pantas untukmu.
+
+Terima kasih sudah membaca.
+
+- Ervan
+`;
+
+let index = 0;
 
 mulai.onclick = () => {
+
     loading.style.display = "none";
     hero.style.display = "flex";
 
+    judul.innerHTML = title;
+
     ketik();
+
 };
 
 function ketik(){
 
-    if(i < text.length){
+    if(index < text.length){
 
-        typing.innerHTML += text.charAt(i);
+        typing.innerHTML += text.charAt(index);
 
-        i++;
+        index++;
 
         setTimeout(ketik,40);
+
+    }else{
+
+        lanjut.style.display="inline-block";
 
     }
 
 }
+
+lanjut.onclick = () => {
+
+    hero.style.display="none";
+    surat.style.display="flex";
+
+    isiSurat.innerText=suratText;
+
+};
